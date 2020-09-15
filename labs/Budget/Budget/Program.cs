1c7f8bee0bfd396093bb2618fb725f4808b2d4ea
Oscar Peinado-Rojo <oscar.peinado-rojo@my.tccd.edu>
2020-09-15 16:12:05 -0500

    Started Lab 1 Story 1

---
+++ labs/Budget/Budget/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+/*
+ *Oscar Peinado-Rojo
+ *ITSE 1430
+ *Lab 1
+ */
+
+
+namespace Budget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program Budget developed by Oscar Peinado. ");
+
+        }
+    }
+
+}
