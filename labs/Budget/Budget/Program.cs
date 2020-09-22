57da8ff54b41052e8429663fc498aee6482d2c4e
Oscar Peinado-Rojo <oscar.peinado-rojo@my.tccd.edu>
2020-09-21 19:30:10 -0500

    Added to Story 2

--- labs/Budget/Budget/Program.cs
+++ labs/Budget/Budget/Program.cs
@@ -10,11 +10,84 @@ namespace Budget
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main ( string[] args )
         {
-            Console.WriteLine("Program Budget developed by Oscar Peinado. ");
+            Console.WriteLine("Budget program developed by Oscar Peinado. ");
+
+
+            Console.WriteLine("Please input account nickname: ");
+
+            accountName = ReadString(true);
+
+            Console.WriteLine("Please input account number: ");
+
+            accountNumber = isDigitOnly(true);
+
+            Console.WriteLine("Please input starting balance: ");
+
+            accountBalance = ReadInt32(0);
+
 
         }
-    }
 
+        static string isDigitOnly ( bool required )
+        {
+            do
+            {
+                string value = Console.ReadLine();
+
+              // if (int)
+            } while (true);
+        }
+
+        static decimal ReadInt32 ()
+        {
+            return ReadInt32(Int32.MinValue);
+        }
+
+        static decimal ReadInt32 ( decimal minimumValue )
+        {
+            do
+            {
+                string value = Console.ReadLine();
+
+                if (Decimal.TryParse(value, out decimal result) && result > minimumValue)
+                    return result;
+
+                if (minimumValue != Int32.MinValue)
+                    DisplayError("Value must be greater than " + minimumValue);
+                else
+                    DisplayError("Must be integral value");
+            } while (true);
+        }
+
+        static string accountName = "";
+        static string accountNumber = "";
+        static decimal accountBalance = 0;
+
+        static string ReadString ( bool required )
+        {
+            do
+            {
+                string value = Console.ReadLine();
+
+                if (!required || value != "")
+                    return value;
+
+                DisplayError("Value is required");
+            } while (true);
+        }
+
+        private static void DisplayError ( string message )
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine(message);
+
+            Console.ResetColor();
+        }
+
+
+    }
 }
