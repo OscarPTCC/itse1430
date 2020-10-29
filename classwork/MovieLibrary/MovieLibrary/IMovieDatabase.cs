05e3f9bb816480cf6593b044951dc54c0c73a391
Oscar Peinado-Rojo <oscar.peinado-rojo@my.tccd.edu>
2020-10-29 17:41:50 -0500

    added MemoryMovieDatabase.

--- classwork/MovieLibrary/MovieLibrary/IMovieDatabase.cs
+++ classwork/MovieLibrary/MovieLibrary/IMovieDatabase.cs
@@ -16,12 +16,25 @@ namespace MovieLibrary
     //  ICompare<T> - relational comparison, StringCompare
     //  ICloneable - clone objects, but doesn't actually work
     //  IValidatableObject - validates objects
+    /// <summary>Provides an interaface for storing and retrieving movies.</summary>
     public interface IMovieDatabase
     {
+        /// <summary>Adds a movie to the database.</summary>
+        /// <param name="movie">The movie to add.</param>
+        /// <param name="error">The error message, if any.</param>
+        /// <returns>The new movie.</returns>
+        /// error: Movie is invalid
+        /// error: Movie already exists
         Movie Add ( Movie movie, out string error );
+
+        /// <summary>Deletes a movie from the database.</summary>
+        /// <param name="id">The movie to be deleted.</param>
         void Delete ( int id );
+
         Movie Get ( int id );
+
         IEnumerable<Movie> GetAll ();
+
         string Update ( int id, Movie movie );
     }
 }
--- classwork/MovieLibrary/MovieLibrary/Memory/MemoryMovieDatabase.cs
+++ classwork/MovieLibrary/MovieLibrary/Memory/MemoryMovieDatabase.cs
@@ -2,83 +2,10 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace MovieLibrary
+namespace MovieLibrary.Memory
 {
-    public class MovieDatabase : IMovieDatabase
+    public class MemoryMovieDatabase : MovieDatabase
     {
-        //Default constructor to seed database
-        protected MovieDatabase ()
-        {
-            //Not needed here
-            //_movies.Clear();
-
-            //Collections initializer syntax
-            var items = new[] { //Movie[] {
-                new Movie() {
-                    Name = "Jaws",
-                    ReleaseYear = 1977,
-                    RunLength = 190,
-                    Description = "Shark movie",
-                    IsClassic = true,
-                    Rating = "PG",
-                },
-                new Movie() {
-                    Name = "Shrek",
-                    ReleaseYear = 1999,
-                    RunLength = 150,
-                    Description = "Ogre movie",
-                    IsClassic = true,
-                    Rating = "PG",
-                }
-            };
-
-            foreach (var item in items)
-                Add(item, out var error);
-            #region seed database
-            //Seed database
-            // Object initialize - only usable on new operator
-            //  1. Can only set properties with setters
-            //  2. Can set properties that are themselves new'ed up but cannot set child properties
-            //      Position = new Position() (Name = "Boss" ); //Allowed
-            //      Position.Title = "Boss"; //Not allowed
-            //  3. Properties cannot rely on other properties
-            //      Description = "blah"; Not allowed
-            //      FullDescription = Description;
-            //var movie = new Movie();
-            //movie.Name = "Jaws";
-            //movie.ReleaseYear = 1977;
-            //movie.RunLength = 190;
-            //movie.Description = "Shark movie";
-            //movie.IsClassic = true;
-            //movie.Rating = "PG";
-            //Add(movie, out var error);
-            //var movie = new Movie() {
-            //    Name = "Jaws",
-            //    ReleaseYear = 1977,
-            //    RunLength = 190,
-            //    Description = "Shark movie",
-            //    IsClassic = true,
-            //    Rating = "PG",
-            //};
-
-            //Add(movie, out var error);
-
-            //movie = new Movie() {
-            //    Name = "Shrek",
-            //    ReleaseYear = 1999,
-            //    RunLength = 150,
-            //    Description = "Ogre movie",
-            //    IsClassic = true,
-            //    Rating = "PG",
-            //};
-
-            //Add(movie, out error);
-            #endregion 
-        }
-
-        //Not on interface
-        public void foo () { }
-
         public Movie Add ( Movie movie, out string error )
         {
             //TODO: Movie is valid
@@ -261,8 +188,10 @@ namespace MovieLibrary
             target.IsClassic = source.IsClassic;
             target.Description = source.Description;
         }
+
         //Only store closed copies of movies here!!
         //private Movie[] _movies = new Movie[100];
+
         private List<Movie> _movies = new List<Movie>();
         private int _id = 1;
 
